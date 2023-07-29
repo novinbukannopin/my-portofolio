041b1a7a54537fb595ddeea53493c52d96e36ca9
Achmad96 <totsukamail3278@gmail.com>
2023-07-29 12:53:20 +0700

    modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,13 @@ import "./App.css";
 import Content from "./components/content";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
+import React, { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    document.title = "Achmad's Portofolio";
+  }, []);
+
   return (
     <div>
       <Navbar />

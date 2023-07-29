93aa3f05f6c6705fcc168ccb34e43c3cdf38d4d5
Achmad96 <totsukamail3278@gmail.com>
2023-07-29 14:34:39 +0700

    modified about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,19 +1,24 @@
+import { Fade } from "react-reveal";
 export default function About() {
   return (
     <div className="about" id="about">
       <div className="card">
-        <img
-          className="img-profile"
-          src="https://avatars.githubusercontent.com/u/73148419?s=400&u=172e9812e0a3e57e3cd6aef1565178b5e12e28d5&v=4"
-          alt="profile"
-        />
-        <div className="description">
-          <h3>Hi there</h3>
-          <p>
-            I'm Achmad, I'm beginner developer. Now I study at University Islam
-            Negeri Sunan Ampel.
-          </p>
-        </div>
+        <Fade left>
+          <img
+            className="img-profile"
+            src="https://avatars.githubusercontent.com/u/73148419?s=400&u=172e9812e0a3e57e3cd6aef1565178b5e12e28d5&v=4"
+            alt="profile"
+          />
+        </Fade>
+        <Fade bottom>
+          <div className="description">
+            <h3>Hi there</h3>
+            <p>
+              I'm Achmad, I'm beginner developer. Now I study at University
+              Islam Negeri Sunan Ampel.
+            </p>
+          </div>
+        </Fade>
       </div>
     </div>
   );

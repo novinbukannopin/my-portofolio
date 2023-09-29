8809c93e9e02473c011a284f9eeb7280d27db3f9
Achmad96 <totsukamail3278@gmail.com>
2023-09-29 19:05:04 +0700

    modified navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -37,7 +37,7 @@ export default function Navbar({ scrollToELement }) {
   return (
     <nav className="fixed flex items-center justify-between w-full h-20 z-50">
       <img src="/logo.png" className="w-10 h-10 max-sm:ml-5 ml-24" alt="Logo" />
-      <div className="flex text-xl gap-10 font-extrabold">
+      <div className="flex max-sm:text-sm text-xl gap-10 font-extrabold">
         <Link ref={aboutBtn} to="about" smooth={true} duration={duration} className={activeIndex === 0 ? "active" : ""}>
         <span>01. </span> About
         </Link>
